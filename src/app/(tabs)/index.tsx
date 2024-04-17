3cf83693c781782a6d38dcb52592fe2645c0cc7b
Nikolay kuzichev <kuzkuz51@gmail.com>
2024-04-17 14:31:09 +0300

    post list item component test

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { Text, View } from '../../components/Themed';
+import { Text, View } from "../../components/Themed";
+
+import PostListItem from "@/components/PostListItem";
+
+import posts from "../../../assets/data/posts.json";
+
+const firstPost = posts[0];
 
 export default function NewPost() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+
+      <PostListItem post={firstPost} />
     </View>
   );
 }
@@ -13,11 +21,11 @@ export default function NewPost() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function PostListItem({ post }) {
+  return <Text>{post.content}</Text>;
+}
